7ec108b489961d2c07ebcc4b72a72bf8bed17f2f
mfs-mark <mark@jiangdang.tech>
2016-12-08 23:20:45 +0800

    Update 2016-12-08T15:20:34.020Z

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 var gulp = require("gulp"),
   deploy = require("gulp-gh-pages");
+  
+var file = require('gulp-file');
 
 gulp.task('deploy', function () {
   return gulp.src("_book/**/*.*")
+    .pipe(file('CNAME', "fe-base.books.mafengshe.com"))
     .pipe(deploy({cname:"fe-base.books.mafengshe.com"}))
     .on("error", function(err){
       console.log(err)
